2f708c7902d60c35a37ee2dd0aac7033da6e8f53
luovkle <roberto.silva.dev@protonmail.com>
2023-10-06 16:01:54 -0600

    ✨ Add CORS config

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,6 +1,14 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 
 from app.api.v1.api import api_router
 
 app = FastAPI()
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["GET"],
+    allow_headers=["*"],
+    allow_credentials=True,
+)
 app.include_router(api_router)

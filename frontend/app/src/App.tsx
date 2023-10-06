540aed84dd1f7206321a020cca5fc95255b0dd58
luovkle <roberto.silva.dev@protonmail.com>
2023-10-06 16:21:26 -0600

    ✨ Add movie rendering

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -1,3 +1,22 @@
+import axios from "axios";
+import { useState, useEffect } from "react";
+
 export const App = () => {
-  return <div>Index</div>;
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    axios
+      .get("http://localhost:8000/api/v1/discover?page=4")
+      .then(({ data: { response } }) => {
+        setMovies(response.results);
+      });
+  }, []);
+
+  return (
+    <>
+      {movies.map((movie) => (
+        <div key={movie.id}>{JSON.stringify(movie)}</div>
+      ))}
+    </>
+  );
 };

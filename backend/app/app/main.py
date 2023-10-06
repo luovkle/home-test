f56583605ee60c70cc4d7415c1386e052e272bc3
luovkle <roberto.silva.dev@protonmail.com>
2023-10-06 14:34:42 -0600

    ✨ Add initial implementation of endpoints

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,8 +1,27 @@
+import os
+
+import requests
 from fastapi import FastAPI
+
+ACCESS_TOKEN = os.getenv("ACCESS_TOKEN")
+
+api_url = "https://api.themoviedb.org"
+headers = {"Authorization": f"Bearer {ACCESS_TOKEN}"}
 
 app = FastAPI()
 
 
-@app.get("/")
-def index():
-    return {"msg": "ok"}
+# List movies sorted by popularity
+@app.get("/discover")
+def discover(page: int):
+    response = requests.get(
+        api_url + f"/3/discover/movie?page={page}&sort_by=popularity.desc",
+        headers=headers,
+    )
+    return {"response": response.json()}
+
+
+# Search movies by title
+@app.get("/search")
+def search(title: str):
+    return {"msg": {"title": title}}

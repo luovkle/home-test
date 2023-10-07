e69e594c4df39ad5f4b162e21c93bd7f8a9a6012
luovkle <roberto.silva.dev@protonmail.com>
2023-10-07 12:26:22 -0600

    ✨ Add static content sub-application

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,5 +1,6 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
+from fastapi.staticfiles import StaticFiles
 
 from app.api.v1.api import api_router
 
@@ -12,3 +13,4 @@ app.add_middleware(
     allow_credentials=True,
 )
 app.include_router(api_router)
+app.mount("/static", StaticFiles(directory="static"), name="static")

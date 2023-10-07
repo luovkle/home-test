01da20f2ea5072cd9627406dae407844773d57e3
luovkle <roberto.silva.dev@protonmail.com>
2023-10-07 17:59:28 -0600

    ✨ Add WebSocket manager

--- backend/app/app/api/v1/endpoints/search.py
+++ backend/app/app/api/v1/endpoints/search.py
@@ -1,10 +1,28 @@
 import requests
-from fastapi import APIRouter, WebSocket
+from fastapi import APIRouter, WebSocket, WebSocketDisconnect
 
 from app.core.config import settings
 
 api_url = "https://api.themoviedb.org"
 headers = {"Authorization": f"Bearer {settings.TMDB_ACCESS_TOKEN}"}
+
+
+class ConnectionManager:
+    def __init__(self):
+        self.active_connections: list[WebSocket] = []
+
+    async def connect(self, websocket: WebSocket):
+        await websocket.accept()
+        self.active_connections.append(websocket)
+
+    def disconnect(self, websocket: WebSocket):
+        self.active_connections.remove(websocket)
+
+    async def send(self, message: dict, websocket: WebSocket):
+        await websocket.send_json(message)
+
+
+manager = ConnectionManager()
 
 router = APIRouter()
 
@@ -12,11 +30,14 @@ router = APIRouter()
 # Search movies by title
 @router.websocket("/ws")
 async def websocket_endpoint(websocket: WebSocket):
-    await websocket.accept()
-    while True:
-        title = await websocket.receive_text()
-        response = requests.get(
-            api_url + f"/3/search/movie?query={title}",
-            headers=headers,
-        )
-        await websocket.send_json(response.json())
+    await manager.connect(websocket)
+    try:
+        while True:
+            title = await websocket.receive_text()
+            response = requests.get(
+                api_url + f"/3/search/movie?query={title}",
+                headers=headers,
+            )
+            await manager.send(response.json(), websocket)
+    except WebSocketDisconnect:
+        manager.disconnect(websocket)

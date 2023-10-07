fda88ed29dbe65abeda0139d2ee6e33ffa016795
luovkle <roberto.silva.dev@protonmail.com>
2023-10-07 12:57:28 -0600

    ✨ Add MongoDB URI to settings

--- backend/app/app/core/config.py
+++ backend/app/app/core/config.py
@@ -7,5 +7,8 @@ class Settings(BaseSettings):
     # TMDB
     TMDB_ACCESS_TOKEN: str = os.getenv("TMDB_ACCESS_TOKEN", "")
 
+    # MongoDB
+    MONGO_URI: str = os.getenv("MONGO_URI", "")
+
 
 settings = Settings()

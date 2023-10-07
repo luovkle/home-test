d430f4a32446581828315bdc55d7d1d811a5b524
luovkle <roberto.silva.dev@protonmail.com>
2023-10-06 18:10:28 -0600

    ✨ Add next button

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -3,20 +3,27 @@ import { useState, useEffect } from "react";
 
 export const App = () => {
   const [movies, setMovies] = useState([]);
+  const [page, setPage] = useState(1);
 
   useEffect(() => {
     axios
-      .get("http://localhost:8000/api/v1/discover?page=4")
+      .get(`http://localhost:8000/api/v1/discover?page=${page}`)
       .then(({ data: { response } }) => {
         setMovies(response.results);
       });
-  }, []);
+  }, [page]);
 
   return (
     <>
       {movies.map((movie) => (
         <div key={movie.id}>{JSON.stringify(movie)}</div>
       ))}
+      <button
+        type="button"
+        onClick={() => setPage((curretPage) => curretPage + 1)}
+      >
+        Next
+      </button>
     </>
   );
 };

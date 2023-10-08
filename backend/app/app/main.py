9829dfc14fcc94a2043805c15e25560b0df196a6
luovkle <roberto.silva.dev@protonmail.com>
2023-10-08 01:55:38 -0600

    🔥 Remove static sub app

--- backend/app/app/main.py
+++ backend/app/app/main.py
@@ -1,6 +1,5 @@
 from fastapi import FastAPI
 from fastapi.middleware.cors import CORSMiddleware
-from fastapi.staticfiles import StaticFiles
 
 from app.api.v1.api import api_router
 from app.db.client import client
@@ -19,7 +18,6 @@ app.add_middleware(
     allow_methods=settings.APP_ALLOW_METHODS,
 )
 app.include_router(api_router)
-app.mount("/static", StaticFiles(directory="static"), name="static")
 
 
 @app.on_event("shutdown")

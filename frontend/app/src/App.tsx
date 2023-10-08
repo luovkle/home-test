52927bd8b532758b76a12fa2003b9b0bd4c8ad90
luovkle <roberto.silva.dev@protonmail.com>
2023-10-08 01:05:45 -0600

    💄 Update general styles

--- frontend/app/src/App.tsx
+++ frontend/app/src/App.tsx
@@ -5,12 +5,12 @@ import { Home, Movie } from "./pages";
 
 export const App = () => {
   return (
-    <>
+    <div className="py-6 px-8">
       <SearchBar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path=":movieId" element={<Movie />} />
       </Routes>
-    </>
+    </div>
   );
 };
--- frontend/app/src/components/MovieCard.tsx
+++ frontend/app/src/components/MovieCard.tsx
@@ -33,7 +33,7 @@ export default function MovieCard({
         </p>
         <Link
           to={`/${id}`}
-          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-700 rounded-lg hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800"
+          className="inline-flex items-center px-3 py-2 text-sm font-medium text-center text-white bg-blue-600 rounded-lg hover:bg-blue-500 focus:ring-4 focus:outline-none focus:ring-blue-300 dark:bg-blue-600 dark:hover:bg-blue-500 dark:focus:ring-blue-800"
         >
           Read more
           <svg

d3c8062e477774fb857fad3812e2703bec17434b
luovkle <roberto.silva.dev@protonmail.com>
2023-10-08 01:05:00 -0600

    💄 Update Movie styles

--- frontend/app/src/pages/Movie.tsx
+++ frontend/app/src/pages/Movie.tsx
@@ -47,32 +47,68 @@ export default function Movie() {
   }, [movieId]);
 
   return (
-    <>
-      <button type="button" onClick={() => navigate("/")}>
-        Back
+    <div className="w-full flex justify-center p-16  text-white">
+      <button
+        type="button"
+        onClick={() => navigate("/")}
+        className="fixed bottom-4 left-1/2 transform -translate-x-1/2 space-x-2 font-semibold text-lg bg-blue-600 hover:bg-blue-500 py-2 px-4 rounded-xl"
+      >
+        {"<< Back"}
       </button>
-      <img
-        className="rounded-t-lg"
-        src={pictureURL}
-        alt={movie.original_title}
-      />
-      <h2>{movie.original_title}</h2>
-      <h3>{movie.release_date}</h3>
-      <hr />
-      <p>{movie.overview}</p>
-      <hr />
-      <ul>
-        {movie.genres &&
-          movie.genres.map((genre) => <li key={genre.id}>{genre.name}</li>)}
-      </ul>
-      <span>{movie.vote_average}</span>
-      <hr />
-      <ul>
-        {movie.spoken_languages &&
-          movie.spoken_languages.map((lang) => (
-            <ul key={lang.english_name}>{lang.english_name}</ul>
-          ))}
-      </ul>
-    </>
+      <div className="flex">
+        <img
+          className="rounded-xl object-scale-down"
+          src={pictureURL}
+          alt={movie.original_title}
+        />
+        <div className="pt-4 px-8">
+          <h2 className="text-3xl font-semibold">{movie.original_title}</h2>
+          <div>
+            <span className="text-xs font-semibold">Release</span>
+            <h3>{movie.release_date}</h3>
+          </div>
+          <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
+          <div>
+            <span className="text-xs font-semibold">Overview</span>
+            <p>{movie.overview}</p>
+          </div>
+          <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
+          <div>
+            <span className="text-xs font-semibold">Genres</span>
+            <ul className="flex space-x-2">
+              {movie.genres &&
+                movie.genres.map((genre) => (
+                  <li
+                    key={genre.id}
+                    className="bg-blue-600 px-4 py-1 rounded-lg"
+                  >
+                    {genre.name}
+                  </li>
+                ))}
+            </ul>
+          </div>
+          <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
+          <div>
+            <span className="text-xs font-semibold">Vote averange</span>
+            <div>{movie.vote_average}</div>
+          </div>
+          <hr className="h-px my-4 bg-gray-200 border-0 dark:bg-gray-700" />
+          <div>
+            <span className="text-xs font-semibold">Langs</span>
+            <ul className="flex space-x-2">
+              {movie.spoken_languages &&
+                movie.spoken_languages.map((lang) => (
+                  <ul
+                    key={lang.english_name}
+                    className="bg-blue-600 px-4 py-1 rounded-lg"
+                  >
+                    {lang.english_name}
+                  </ul>
+                ))}
+            </ul>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }

1e1d57f70a4a0edc34cff9017c6875d04422e37f
luovkle <roberto.silva.dev@protonmail.com>
2023-10-07 23:51:34 -0600

    🎨 Add styles to navigation buttons

--- frontend/app/src/pages/Home.tsx
+++ frontend/app/src/pages/Home.tsx
@@ -8,18 +8,25 @@ export default function Home() {
   return (
     <>
       <MovieGrid page={page} />
-      <button
-        type="button"
-        onClick={() => setPage((curretPage) => curretPage - 1)}
-      >
-        Back
-      </button>
-      <button
-        type="button"
-        onClick={() => setPage((curretPage) => curretPage + 1)}
-      >
-        Next
-      </button>
+      <div className="fixed bottom-4 left-1/2 transform -translate-x-1/2 space-x-2">
+        <button
+          type="button"
+          onClick={() => setPage((curretPage) => curretPage - 1)}
+          className={
+            "font-semibold text-lg py-2 px-4 rounded-xl" +
+            (page > 1 ? " bg-sky-500 hover:bg-sky-400" : " bg-sky-600")
+          }
+        >
+          {"<< Back"}
+        </button>
+        <button
+          type="button"
+          onClick={() => setPage((curretPage) => curretPage + 1)}
+          className="font-semibold text-lg bg-sky-500 hover:bg-sky-400 py-2 px-4 rounded-xl"
+        >
+          {"Next >>"}
+        </button>
+      </div>
     </>
   );
 }
